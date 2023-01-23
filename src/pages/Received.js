9ae9307c6da77fb18df232e4fe8e811b002ce169
Pete Laptop <pmugasa78@gmail.com>
2023-01-23 20:09:26 +0200

    edited badges to be on two separate line

--- src/pages/Received.js
+++ src/pages/Received.js
@@ -4,49 +4,60 @@ const Received = () => {
       shipmentId: "#001",
       date: "Jan 23, 2023",
       weight: "0.5kg",
-      img: "Takealot",
+      img: "https://placeimg.com/192/192/people",
       trackingId: " EV938507560CN",
     },
     {
       shipmentId: "#002",
       date: "Jan 23, 2023",
       weight: "0.8kg",
-      img: "Mr Price",
+      img: "https://placeimg.com/192/192/people",
       trackingId: "175030116230104059",
     },
     {
       shipmentId: "#003",
       date: "Jan 23, 2023",
       weight: "1.0kg",
-      img: "H&M",
+      img: "https://placeimg.com/192/192/people",
       trackingId: "9505511597628205266397",
     },
   ];
 
   return (
     <>
-      <div className="flex flex-col w-screen px-2">
-        <div className="border-2 border-gray-200 rounded-lg  w-full h-28 mx-auto mt-8 p-2 flex flex-col">
-          <div className="flex justify-center items-start">
-            <div className="avatar my-auto">
-              <div className="w-20 rounded">
-                <img src="https://placeimg.com/192/192/people" alt="parcel" />
+      {parcels.map((parcel) => {
+        return (
+          <div className="flex flex-col w-screen px-2">
+            <div className="border-2 border-gray-200 rounded-lg  w-full h-28 mx-auto mt-8 p-2 flex flex-col">
+              <div className="flex justify-start items-start">
+                <div className="avatar my-auto flex-none">
+                  <div className="w-20 rounded">
+                    <img src={parcel.img} alt="parcel" />
+                  </div>
+                </div>
+
+                <div className="mx-4  flex-none w-40 ">
+                  <p className="text-base font-bold">
+                    Shipment: {parcel.shipmentId}
+                  </p>
+                  <div className="space-x-2 ">
+                    <p className="badge badge-ghost badge-sm">
+                      {parcel.weight}
+                    </p>
+                    <p className="badge badge-ghost badge-sm">{parcel.date}</p>
+                    <p className="badge badge-ghost badge-sm block mt-2 ">
+                      {parcel.trackingId}
+                    </p>
+                  </div>
+                </div>
+                <div className="ml-auto mt-auto">
+                  <button className="btn btn-sm">PACK</button>
+                </div>
               </div>
             </div>
-
-            <div className="mx-4 ">
-              <p className="text-base font-bold">Shipment ID: #001</p>
-              <div className="space-x-1">
-                <p className="badge badge-ghost badge-sm">0.5kg</p>
-                <p className="badge badge-ghost badge-sm">Jan 23, 2023</p>
-                <p className="badge badge-ghost badge-sm">EV938507560CN</p>
-              </div>
-            </div>
-
-            <button className="btn btn-sm  mt-auto">PACK</button>
           </div>
-        </div>
-      </div>
+        );
+      })}
     </>
   );
 };
